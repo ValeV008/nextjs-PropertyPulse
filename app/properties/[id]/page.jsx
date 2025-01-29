2fc2634798536427adc21c33070db9ff3aa2a727
Valentin Kragelj <valentin.kragelj@endava.net>
2025-01-29 11:36:16 +0100

    12. Show some client component hooks examples.

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,8 +1,29 @@
+'use client';
+
 // [id] means dynamic routing. Whatever url we go to http://localhost:3000/properties/100 or http://localhost:3000/properties/42, we will get this page.
 // '[...id]' is a 'catch all', meaning if we put any subroute (i.e. ".../100/rooms"), we would still get this page.
 
+import { useRouter, useParams, useSearchParams, usePathname } from "next/navigation";
+
 const PropertyPage = () => {
-    return ( <div>Property Page</div> );
+    console.log("Property page log"); {/* Since PropertyPage is a server component (without 'use client' at the top), the log will show in server terminal, not in browser console */}
+
+    const router = useRouter();
+    const params = useParams();
+    const searchParams = useSearchParams();
+    const pathName = usePathname();
+    return ( <div>
+        Property Page
+        <br /><br />
+        <button onClick={() => router.replace('/')}>UseRouter: Go Home</button>
+        <br />
+        UseParams: Dynamic part: {params.id} {/* http://localhost:3000/properties/100 -> params.id == 100. id because folder name is also [id]. */}
+        <br />
+        UseSearchParams: Search params: {searchParams.get("name")} {/* http://localhost:3000/properties/100?name=Valentin */}
+        <br />
+        UsePathname: Pathname: {pathName} {/* http://localhost:3000/properties/100?name=Valentin -> /properties/100 */}
+    </div> );
+
 }
  
 export default PropertyPage;
