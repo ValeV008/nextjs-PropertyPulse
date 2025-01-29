151862d2da26469a74bc6efae42aa2c35a3faec0
Valentin Kragelj <valentin.kragelj@endava.net>
2025-01-29 11:08:38 +0100

    Fixed commend syntax.

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 const HomePage = () => {
     return ( <div>
         <h1 className='text-3xl'>Welcome</h1>
-        <Link href='/properties'>Go to Properties</Link> //link is client-side routing, so it doesn't refresh the page
+        <Link href='/properties'>Go to Properties</Link> {/* <link> is client-side routing, so it doesn't refresh the page */}
     </div> );
 }
  
