ab2ce012df40a629284ba41bc90d0ff673d42f23
Valentin Kragelj <valentin.kragelj@endava.net>
2025-02-03 12:44:52 +0100

    22. Create function to connect to db.

---
+++ config/database.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+let connected = false;
+
+const connectDB = async () => {
+    mongoose.set('strictQuery', true); //save to db only specified fields
+
+    if (connected){
+        console.log('MongoDB is connected');
+        return;
+    }
+
+    try {
+        await mongoose.connect(process.env.MONGODB_URI);
+        connected = true;
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export default connectDB;
