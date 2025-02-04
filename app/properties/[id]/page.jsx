91dc2bb96defa075298eee3bf283583fc81422f2
Valentin Kragelj <valentin.kragelj@endava.net>
2025-02-04 15:29:39 +0100

    25. Added comment.

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -23,7 +23,7 @@ const PropertyPage = async ({params}) => {
         </section>
         <section className="bg-blue-50">
             <div className="container m-auto py-10 px-6">
-                <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
+                <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6"> {/* we defined 'grid-cols-70/30' in tailwind.config.mjs */}
                     {/* property info */}
                 </div>
             </div>
