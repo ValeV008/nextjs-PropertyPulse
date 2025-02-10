2e09e6e39f116461175b2d328113983b3eeca91f
Valentin Kragelj <valentin.kragelj@endava.net>
2025-02-10 11:51:31 +0100

    32. Save user to db on sign in.

--- utils/authOptions.js
+++ utils/authOptions.js
@@ -1,4 +1,6 @@
 import GoogleProvider from "next-auth/providers/google";
+import connectDB from "@/config/database";
+import User from "@/models/User";
 
 export const authOptions = {
     providers: [
@@ -14,20 +16,35 @@ export const authOptions = {
             }
         })
     ],
-    callback: {
+    callbacks: {
         //invoked on successfull sign in
         async signIn({profile}) {
             // connect to database
+            await connectDB();
             // check if user exists
+            const userExists = await User.findOne({email: profile.email});
             // if not, create user
+            if (!userExists) {
+                // truncate username if too long
+                const username = profile.name.slice(0, 20);
+                await User.create({
+                    email: profile.email,
+                    username,
+                    image: profile.picture
+                });
+            }
             // return true to allow sign in
+            return true;
         },
 
         // session callback function that modifies the session object
         async session({session}) {
             // get user from database
+            const user = await User.findOne({email: session.user.email});
             // assing user id from the session
+            session.user.id = user._id.toString();
             // return session
+            return session;
         }
     }
 }
