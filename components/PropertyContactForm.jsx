b5104efcf1e110c52ded67ba3931d0d3aadf6cf0
Valentin Kragelj <valentin.kragelj@endava.net>
2025-03-04 12:59:11 +0100

    65. Submit message functionality.

--- components/PropertyContactForm.jsx
+++ components/PropertyContactForm.jsx
@@ -1,10 +1,39 @@
+'use client'
+
 import { FaPaperPlane } from "react-icons/fa";
+import { useEffect } from "react";
+import { useActionState, useFormStatus } from "react";
+import { useSession } from "next-auth/react";
+import { toast } from "react-toastify";
+import addMessage from "@/app/actions/AddMessage";
 
 const PropertyContactForm = ({property}) => {
-    return (
+    const {data: session} = useSession();
+
+    const [state, formAction] = useActionState(addMessage, {});
+
+    useEffect(() => {
+        if (state.error) // can be returned by addMessage()
+            toast.error(state.error);
+        if (state.submitted)
+            toast.success("Message sent successfully.");
+    },[state])
+
+    if (state.submitted){ // 'submitted' is returned by addMessage()
+        return (
+            <div className="bg-white p-6 rounded-lg shadow-md">
+            <h3 className="text-xl font-bold mb-6">Contact Property Manager</h3>
+            <p className="text-green-500 mb-4 text-center">"Your message has been sent"</p>
+            </div>
+        );
+    }
+
+    return session && (
         <div className="bg-white p-6 rounded-lg shadow-md">
               <h3 className="text-xl font-bold mb-6">Contact Property Manager</h3>
-              <form>
+              <form action={formAction}>
+                <input type="hidden" id="property" name="property" defaultValue={property._id}/>
+                <input type="hidden" id="recipient" name="recipient" defaultValue={property.owner}/>
                 <div className="mb-4">
                   <label
                     className="block text-gray-700 text-sm font-bold mb-2"
@@ -55,14 +84,14 @@ const PropertyContactForm = ({property}) => {
                 <div className="mb-4">
                   <label
                     className="block text-gray-700 text-sm font-bold mb-2"
-                    htmlFor="message"
+                    htmlFor="body"
                   >
                     Message:
                   </label>
                   <textarea
                     className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 h-44 focus:outline-none focus:shadow-outline"
-                    id="message"
-                    name="message"
+                    id="body"
+                    name="body"
                     placeholder="Enter your message"
                   ></textarea>
                 </div>
