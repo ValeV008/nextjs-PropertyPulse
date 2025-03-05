a11717021c430ebbc40a1f62e199b0c168e8a12a
Valentin Kragelj <valentin.kragelj@endava.net>
2025-03-05 10:21:01 +0100

    73. Create pagination buttons.

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,19 +1,22 @@
 import PropertyCard from '@/components/PropertyCrd';
 import connectDB from '@/config/database';
 import Property from '@/models/Property';
+import Pagination from '@/components/Pagination';
 
 const PropertiesPage = async ({ searchParams }) => {
-    const { page = 1, pageSize = 2 } = await searchParams;
+    const { page = 1, pageSize = 9 } = await searchParams;
     await connectDB();
 
     const skip = (page-1) * pageSize;
     const total = await Property.countDocuments({});
     const properties = await Property.find({}).skip(skip).limit(pageSize);
 
+    const showPagination = total > pageSize;
+
     return (
         <section className='px-4 py-6'>
             <div className='container-xl lg:container m-auto px-4 py-6'>
-                { properties.length == 0 ? (<p>No properties found</p>) : (
+                { properties.length == 0 ? (<p>No properties found on this page.</p>) : (
                     <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
                         {
                             properties.map((property) => (
@@ -21,7 +24,8 @@ const PropertiesPage = async ({ searchParams }) => {
                             ))
                         }
                     </div>
-                ) }
+                )}
+                {showPagination && (<Pagination page={parseInt(page)} pageSize={parseInt(pageSize)} totalItems={total} />)}
             </div>
         </section>
     );
